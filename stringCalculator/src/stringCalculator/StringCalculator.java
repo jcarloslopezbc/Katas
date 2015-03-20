dc2f4182c4c2712a356a24b3ba970179482cd1b6
carloslopez <carloslopez@carloslopez-HP>
2015-03-20 14:47:11 -0600

    soporte a diferentes delimitadores iniciando con //[Delimitador]

--- stringCalculator/src/stringCalculator/StringCalculator.java
+++ stringCalculator/src/stringCalculator/StringCalculator.java
@@ -2,18 +2,28 @@ package stringCalculator;
 
 public class StringCalculator {
 	
+	
+	
 	public int add(String string){
 		if (string.length() == 0)
 		return 0;
-	return suma(string.split(",|\n"));
+		String delimitador = ",|\n";
+		if (string.startsWith("//"))
+			delimitador = string.substring(2,3);
+		String valores = string;
+		if (string.startsWith("//"))
+			valores  = string.substring(4);
+		
+	return suma(valores.split(delimitador));
 	}
 
-	private int suma(String[] valoresCadena) {
+	private int suma(String[] valorescadena) {
 		int total = 0;
-		for (String valor : valoresCadena) {
+		for (String valor : valorescadena) {
 			total += Integer.valueOf(valor);
 		}
 		return total;
 	}
+	
 
 }
--- stringCalculator/src/stringCalculator/StringCalculatorTest.java
+++ stringCalculator/src/stringCalculator/StringCalculatorTest.java
@@ -6,15 +6,24 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	private StringCalculator calculator = new StringCalculator();
 	@Test
 	public void emptyString() {
-		StringCalculator calculator = new StringCalculator();
+		
 		assertEquals(0, calculator.add(""));
 		assertEquals(1, calculator.add("1"));
 		assertEquals(2, calculator.add("2"));
 		assertEquals(4, calculator.add("2,2"));
 		assertEquals(7, calculator.add("2,2,3"));
 		assertEquals(6, calculator.add("1\n2\n3"));
+		
+		
+	}
+	
+	@Test
+	public void newDelimiter(){
+		assertEquals(3, calculator.add("//,\n1,2"));
+		assertEquals(3, calculator.add("//;\n1;2"));
 	}
 		
 	
