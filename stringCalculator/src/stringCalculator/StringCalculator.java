313e4c4dc2d0b60729c32b94998daeecca374a5c
carloslopez <carloslopez@carloslopez-HP>
2015-03-20 14:56:50 -0600

    Refactor

--- stringCalculator/src/stringCalculator/StringCalculator.java
+++ stringCalculator/src/stringCalculator/StringCalculator.java
@@ -2,19 +2,27 @@ package stringCalculator;
 
 public class StringCalculator {
 	
-	
+	private String delimitador = ",|\n";
 	
 	public int add(String string){
 		if (string.length() == 0)
 		return 0;
-		String delimitador = ",|\n";
-		if (string.startsWith("//"))
-			delimitador = string.substring(2,3);
+		asignarDelimitador(string);
 		String valores = string;
-		if (string.startsWith("//"))
-			valores  = string.substring(4);
+		valores = determinarValores(string, valores);
 		
 	return suma(valores.split(delimitador));
+	}
+
+	private String determinarValores(String string, String valores) {
+		if (string.startsWith("//"))
+			valores  = string.substring(4);
+		return valores;
+	}
+
+	private void asignarDelimitador(String string) {
+		if (string.startsWith("//"))
+			delimitador = string.substring(2,3);
 	}
 
 	private int suma(String[] valorescadena) {
