de87ec53c4d40c698c463cc50cce462f68d4b1e6
carloslopez <carloslopez@carloslopez-HP>
2015-03-20 13:51:07 -0600

    Refactor

--- stringCalculator/src/stringCalculator/StringCalculator.java
+++ stringCalculator/src/stringCalculator/StringCalculator.java
@@ -5,12 +5,20 @@ public class StringCalculator {
 	public int add(String string){
 		if (string.length() == 0)
 		return 0;
-	String[] valoresCadena = string.split(",");
-	int total = 0;
-	for (String valor : valoresCadena) {
-		total += Integer.valueOf(valor);
-	}
+	int total = suma(string);
 	return total;
+	}
+
+	private int suma(String string) {
+		return suma(string.split(","));
+	}
+
+	private int suma(String[] valoresCadena) {
+		int total = 0;
+		for (String valor : valoresCadena) {
+			total += Integer.valueOf(valor);
+		}
+		return total;
 	}
 
 }
