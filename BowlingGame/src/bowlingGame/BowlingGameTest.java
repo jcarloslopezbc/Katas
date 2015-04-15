0e06bd4adaa04b98665e367a7675718652893052
carloslopez <carloslopez@carloslopez-HP>
2015-04-15 11:45:15 -0500

    Refactor

--- BowlingGame/src/bowlingGame/BowlingGameTest.java
+++ BowlingGame/src/bowlingGame/BowlingGameTest.java
@@ -21,10 +21,8 @@ public class BowlingGameTest {
 	
 	@Test
 	public void allGameGutters(){
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 20; i++)
 			game.roll(0);
-		}
-		
 		assertEquals(0, game.score());
 	}
 	
