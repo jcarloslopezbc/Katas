654de87526da8c7cc3662d993509489861d3cef3
carloslopez <carloslopez@carloslopez-HP>
2015-04-20 10:08:57 -0500

    Test para cuando se hace un spare (failed)

--- BowlingGame/src/bowlingGame/BowlingGameTest.java
+++ BowlingGame/src/bowlingGame/BowlingGameTest.java
@@ -24,14 +24,21 @@ public class BowlingGameTest {
 		loopBowling(0, 0);
 		assertEquals(0, game.score());
 	}
-
-	
 	
 	@Test
 	public void allRollsOne(){
 		loopBowling(20, 1);
 		assertEquals(20, game.score());
 		
+	}
+	
+	@Test
+	public void spare(){
+		game.roll(5);
+		game.roll(5);
+		game.roll(3);
+		loopBowling(17, 0);
+		assertEquals(16, game.score());
 	}
 	
 	private void loopBowling(int n, int pins) {
