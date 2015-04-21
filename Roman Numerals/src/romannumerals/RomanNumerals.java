02fedb3afd78f285de6d813e76017681909e8ed2
carloslopez <carloslopez@carloslopez-HP>
2015-04-21 13:12:10 -0500

    Refactor

--- Roman Numerals/src/romannumerals/RomanNumerals.java
+++ Roman Numerals/src/romannumerals/RomanNumerals.java
@@ -2,20 +2,22 @@ package romannumerals;
 
 public class RomanNumerals {
 
+	private static int[] VALUES = {9, 5, 4 };
+	private static String[] SYMBOLS = {"IX", "V", "IV"};
+	
 	public static String numberToRoman(int number) {
 		StringBuilder result = new StringBuilder();
 		int remainder = number;
-		remainder = collectRomanNumerals(result, remainder, 9,"IX");
-		remainder = collectRomanNumerals(result, remainder, 5,"V");
-		remainder = collectRomanNumerals(result, remainder, 4,"IV");
+		for (int i = 0; i < VALUES.length; i++) {
+			remainder = collectRomanNumerals(result, remainder, VALUES[i],SYMBOLS[i]);	
+		}
 		for (int i = 0; i < remainder; i++) {
 			result.append("I");
 			}
 		return result.toString();
 	}
 
-	private static int collectRomanNumerals(StringBuilder result,
-			int remainder, int arabicNumber, String romanDigits) {
+	private static int collectRomanNumerals(StringBuilder result,int remainder, int arabicNumber, String romanDigits) {
 		if (remainder >= arabicNumber){
 			result.append(romanDigits);
 			remainder -= arabicNumber;
