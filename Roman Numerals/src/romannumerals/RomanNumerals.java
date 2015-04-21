47cccca1a9e2de18dd5800171b21b153dd937574
carloslopez <carloslopez@carloslopez-HP>
2015-04-21 12:23:13 -0500

    Test 6 pasa

--- Roman Numerals/src/romannumerals/RomanNumerals.java
+++ Roman Numerals/src/romannumerals/RomanNumerals.java
@@ -4,10 +4,12 @@ public class RomanNumerals {
 
 	public static String numberToRoman(int number) {
 		StringBuilder result = new StringBuilder();
-		if (number == 5){
+		if (number == 6 ){
+			result.append("VI");
+		}else if (number == 5){
 			result.append("V");
 		} else if (number == 4) {
-			return "IV";
+			result.append("IV");
 		} else{
 			for (int i = 0; i < number; i++) {
 			result.append("I");
