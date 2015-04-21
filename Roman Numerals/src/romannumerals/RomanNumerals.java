6e1fe5226a998dd945e820451fc59eb892fa32eb
carloslopez <carloslopez@carloslopez-HP>
2015-04-21 13:00:27 -0500

    Test 9 pasa y Refactor

--- Roman Numerals/src/romannumerals/RomanNumerals.java
+++ Roman Numerals/src/romannumerals/RomanNumerals.java
@@ -4,20 +4,23 @@ public class RomanNumerals {
 
 	public static String numberToRoman(int number) {
 		StringBuilder result = new StringBuilder();
-		int remainder = 0;
-		remainder = number;
-		if (remainder >= 5){
-			result.append("V");
-			remainder -= 5;
-		}
-		if (remainder == 4) {
-			result.append("IV");
-			remainder -= 4;
-		}
+		int remainder = number;
+		remainder = collectRomanNumerals(result, remainder, 9,"IX");
+		remainder = collectRomanNumerals(result, remainder, 5,"V");
+		remainder = collectRomanNumerals(result, remainder, 4,"IV");
 		for (int i = 0; i < remainder; i++) {
 			result.append("I");
 			}
 		return result.toString();
+	}
+
+	private static int collectRomanNumerals(StringBuilder result,
+			int remainder, int arabicNumber, String romanDigits) {
+		if (remainder >= arabicNumber){
+			result.append(romanDigits);
+			remainder -= arabicNumber;
+		}
+		return remainder;
 	}
 	
 
