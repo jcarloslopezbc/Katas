cc2c020393e4d68472a693599893c899eddd5709
carloslopez <carloslopez@carloslopez-HP>
2015-04-21 11:55:32 -0500

    test de numero 4 pasa

--- Roman Numerals/src/romannumerals/RomanNumerals.java
+++ Roman Numerals/src/romannumerals/RomanNumerals.java
@@ -4,6 +4,9 @@ public class RomanNumerals {
 
 	public static String numberToRoman(int number) {
 		StringBuilder result = new StringBuilder();
+		if (number == 4) {
+			return "IV";
+		}
 		for (int i = 0; i < number; i++) {
 			result.append("I");
 		}
