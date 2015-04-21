7edfafed489537cef067d560858b1cc869e69089
carloslopez <carloslopez@carloslopez-HP>
2015-04-21 11:51:01 -0500

    Test 3 dvuelve III pasa

--- Roman Numerals/src/romannumerals/RomanNumerals.java
+++ Roman Numerals/src/romannumerals/RomanNumerals.java
@@ -4,9 +4,9 @@ public class RomanNumerals {
 
 	public static String numberToRoman(int number) {
 		StringBuilder result = new StringBuilder();
-		if (number == 2)
+		for (int i = 0; i < number; i++) {
 			result.append("I");
-		result.append("I");
+		}
 		return result.toString();
 		
 	}
